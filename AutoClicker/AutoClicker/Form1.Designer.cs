b3e637fa85f01e6cbb4ab11e51a8821dd7fc735a
rickeed <yunustmti@gmail.com>
2023-05-27 20:40:02 +0300

    run with capslock

---
+++ AutoClicker/AutoClicker/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace AutoClicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.Update = new System.Windows.Forms.Timer(this.components);
+            this.Clicker = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(122, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "stop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(24, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 52);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "2";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(24, 76);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(192, 56);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // Update
+            // 
+            this.Update.Interval = 1;
+            this.Update.Tick += new System.EventHandler(this.Update_Tick);
+            // 
+            // Clicker
+            // 
+            this.Clicker.Tick += new System.EventHandler(this.Clicker_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Crimson;
+            this.pictureBox1.Location = new System.Drawing.Point(122, 135);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(78, 36);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(122, 138);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(47, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(245, 176);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Auto Clicker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Timer Update;
+        private System.Windows.Forms.Timer Clicker;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ AutoClicker/AutoClicker/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace AutoClicker
+{
+    public partial class Form1 : Form
+    {
+
+
+        [DllImport("user32.dll")]
+
+        static extern void mouse_event(uint dwFlags, int dx, int dy, uint dwData, UIntPtr dwExtraInfo);
+        
+
+
+
+        const uint LeftD = 0x0002;
+        const uint LeftU = 0x0004;
+     
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Update.Start();
+            Clicker.Stop();
+            label1.Text = trackBar1.Value.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Clicker.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Clicker.Stop();
+        }
+
+        new private void Update_Tick(object sender, EventArgs e)
+        {
+            label1.Text = trackBar1.Value.ToString();
+            Clicker.Interval = trackBar1.Value;
+        }
+        private void Clicker_Tick(object sender, EventArgs e)
+        {
+            if (Control.IsKeyLocked(Keys.CapsLock))
+            {
+                   {
+                        
+                        mouse_event(LeftD, 6, 0, 9, (UIntPtr)0);
+                        mouse_event(LeftU, 6, 0, 9, (UIntPtr)0);
+                    }
+            }
+
+
+               
+            
+            else
+            {
+                return;
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+
+        
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Button btn = (sender as Button);
+            btn.BackColor = Color.Yellow;
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Mouse konumundaki pikselin rengini alın
+            Color color = GetPixelColor(e.X, e.Y);
+
+            // Rengi bir metin olarak gösterin
+            label1.Text = "R: " + color.R.ToString() + " G: " + color.G.ToString() + " B: " + color.B.ToString();
+        }
+
+        public Color GetPixelColor(int x, int y)
+        {
+            // Ekranın tüm boyutlarını alın
+            Rectangle screenSize = new Rectangle(0, 0, Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+
+            // Ekranın görüntüsünü alın
+            Bitmap bmpScreen = new Bitmap(screenSize.Width, screenSize.Height);
+            using (Graphics g = Graphics.FromImage(bmpScreen))
+            {
+                g.CopyFromScreen(0, 0, 0, 0, bmpScreen.Size);
+            }
+
+            // Belirtilen konumdaki pikselin rengini alın
+            Color color = bmpScreen.GetPixel(x, y);
+
+            // Belleği serbest bırakın
+            bmpScreen.Dispose();
+
+            return color;
+        }
+
+    }
+}
